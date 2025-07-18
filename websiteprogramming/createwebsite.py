5cbd57939f031d7f6bcee2a084c84e3ba2ffc209
Shalev Widen <shalevwiden@gmail.com>
2025-07-17 20:33:30 -0500

    Better Organization

--- websiteprogramming/createwebsite.py
+++ websiteprogramming/createwebsite.py
@@ -145,6 +145,113 @@ class createWebsite:
 
 # --------------------------------Degree pages now ----------------------------------
 
+    def upload_files(self):
+
+        '''
+        Like other functions, this does it by school.
+        '''
+         
+        for i in range(1,len(self.schooldata)):
+            '''
+            Dont need to clean the degreename, since the degreename files(csv, excel,etc) already have clean names.
+'''
+
+            key=list(self.schooldata)[i]
+            degreename=key
+            degreename=degreename.replace('/','-').strip()
+
+            degreenameassetfolder=os.path.join(self.assetschoolfolderpath,degreename)
+
+
+            def get_assetlists(degreenameassetfolder):
+                    
+
+                    csvlist=[]
+                    excellist=[]
+                    pdflist=[]
+                    mmdlist=[]
+                    # os.walk recursively travels everything
+                    for root, dirs, files in os.walk(degreenameassetfolder):
+                        for file in files:
+                            
+                            # we neewd the fullpath in the list since thats the way it can be uploaded to google cloud.
+
+                            full_path = os.path.join(root, file)
+
+                            if os.path.splitext(file)[1]=='.csv':
+                                csvlist.append(full_path)
+                                # removes those dollar sign excel files. 
+                            elif os.path.splitext(file)[1]=='.xlsx' and not file.startswith(("~$", "$")):
+                                excellist.append(full_path)
+                            elif os.path.splitext(file)[1]=='.pdf':
+                                pdflist.append(full_path)
+                            elif os.path.splitext(file)[1]=='.mmd':
+                                mmdlist.append(full_path)
+
+                    # these lists have the full paths since they'll be used to upload stuff. 
+                    return [csvlist,excellist,pdflist,mmdlist]
+
+
+            def upload_to_googlecloud(source_file_name):
+
+                # how to manually change credentials...
+
+
+                # just change project name here to change where they go.
+                # In code, for multiple schools. Dope.
+                client = storage.Client(project='degreeview-ut')
+
+
+                # this should return the email used for google cloud. Its a service email tho
+
+                # yeah the project is the same as the bucket name
+                bucket = client.bucket('degreeview-ut')
+                # bucket list
+
+
+            #THis is what will be in the url and what the name of the object will be in google cloud storage
+
+                cleaned_object_name=source_file_name.split('/')[-1]        
+
+
+                # make it so each file has the type. 
+                if os.path.splitext(source_file_name)[1]=='.csv':
+                    uploadblob =f'csvs/{bucket.blob(cleaned_object_name)}'
+                elif os.path.splitext(source_file_name)[1]=='.xlsx':
+                    uploadblob =f'excel-files/{bucket.blob(cleaned_object_name)}'
+
+                elif os.path.splitext(source_file_name)[1]=='.pdf':
+                    uploadblob =f'pdfs/{bucket.blob(cleaned_object_name)}'
+                elif os.path.splitext(source_file_name)[1]=='.mmd':
+                    uploadblob =f'mmds/{bucket.blob(cleaned_object_name)}'
+
+                else:
+                    uploadblob = bucket.blob(cleaned_object_name)
+
+                # add a check to not do it many times
+                if not uploadblob.exists():
+
+                    uploadblob.upload_from_filename(source_file_name)
+
+                    uploadblob.make_public()  # Makes it publicly accessible
+                    # can also use blob.make_private()
+                else:
+                    print(f'{uploadblob.name} already exits, didnt upload\n')
+
+
+                # use this link on the website to serve the file.
+                return uploadblob.public_url
+
+            def loop_through_assets_to_upload():
+                for csv, excelfile,pdf,mmd in zip(get_assetlists(degreenameassetfolder=degreenameassetfolder)):
+                    # comment these out depending on which ones I want
+                    upload_to_googlecloud(csv)
+                    upload_to_googlecloud(excelfile)
+                    upload_to_googlecloud(pdf)
+                    upload_to_googlecloud(mmd)
+
+
+
 
     def create_degree_pages(self):
         '''
@@ -155,19 +262,19 @@ class createWebsite:
             cleaneddegreenamelist=[]
             for i in range(1,len(self.schooldata)):
                         
-                        key=list(self.schooldata)[i]
-                        degreename=key
-                        degreename=degreename.replace('/','-').strip()
-                        degreenamelist.append(degreename)
+                key=list(self.schooldata)[i]
+                degreename=key
+                degreename=degreename.replace('/','-').strip()
+                degreenamelist.append(degreename)
 
-                        # clean it for website links
+                # clean it for website links
 
 
-                        degreenamecleaned=degreename.replace(' ','').lower().split('(')
-                        degreenamecleaned=degreenamecleaned[0]+"-"+degreenamecleaned[-1]
-                        degreenamecleaned=degreenamecleaned.replace(')','')
+                degreenamecleaned=degreename.replace(' ','').lower().split('(')
+                degreenamecleaned=degreenamecleaned[0]+"-"+degreenamecleaned[-1]
+                degreenamecleaned=degreenamecleaned.replace(')','')
 
-                        cleaneddegreenamelist.append(degreenamecleaned)
+                cleaneddegreenamelist.append(degreenamecleaned)
             return degreenamelist,cleaneddegreenamelist
         
         degreenamelist, cleaneddegreenamelist=get_degreename_lists()
@@ -183,39 +290,61 @@ class createWebsite:
 
             # get file types
 
-            def get_degreefiles_lists(degreenameassetfolder):
+            def get_assetcloudpaths_lists(degreenameassetfolder):
                 
+                '''
+                Reconstruct the asset names manually like this:
 
-                csvlist=[]
-                excellist=[]
-                pdflist=[]
-                mmdlist=[]
+                https://storage.googleapis.com/[BUCKET_NAME]/[OBJECT_NAME]
+
+                Unlike other functions, this is by degree not school.
+                As such this will not contain any school specific csv or excel files. 
+                Those have to be obtained with another function. 
+                This function returns the links that will be added to the website. That way I can get the links without a class A operation
+
+                '''
+                csv_path_list=[]
+                excel_path_list=[]
+                pdf_path_list=[]
+                mmd_path_list=[]
                 # os.walk recursively travels everything
                 for root, dirs, files in os.walk(degreenameassetfolder):
                     for file in files:
                         
                         # we neewd the fullpath in the list since thats the way it can be uploaded to google cloud.
 
-                        full_path = os.path.join(root, file)
+                        googlecloudpath=f'https://storage.googleapis.com/degreeview-ut/'
 
                         if os.path.splitext(file)[1]=='.csv':
-                            csvlist.append(file)
+                            objectname_incloud=f'csvs/{file}'
+                            googlecloudpath=f'https://storage.googleapis.com/degreeview-ut/{objectname_incloud}'
+
+                            csv_path_list.append(googlecloudpath)
                             # removes those dollar sign excel files. 
                         elif os.path.splitext(file)[1]=='.xlsx' and not file.startswith(("~$", "$")):
-                            excellist.append(full_path)
-                        elif os.path.splitext(file)[1]=='.pdf':
-                            pdflist.append(file)
-                        elif os.path.splitext(file)[1]=='.mmd':
-                            mmdlist.append(file)
 
-                return [csvlist,excellist,pdflist,mmdlist]
+                            objectname_incloud=f'excel-files/{file}'
+                            googlecloudpath=f'https://storage.googleapis.com/degreeview-ut/{objectname_incloud}'
+                        
+                            excel_path_list.append(googlecloudpath)
+                        elif os.path.splitext(file)[1]=='.pdf':
+                        
+                            objectname_incloud=f'pdfs/{file}'
+                            googlecloudpath=f'https://storage.googleapis.com/degreeview-ut/{objectname_incloud}'
+                        
+                            pdf_path_list.append(googlecloudpath)
+                        elif os.path.splitext(file)[1]=='.mmd':
+                        
+                            objectname_incloud=f'mmds/{file}'
+                            googlecloudpath=f'https://storage.googleapis.com/degreeview-ut/{objectname_incloud}'
+                            mmd_path_list.append(googlecloudpath)
+
+                return [csv_path_list,excel_path_list,pdf_path_list,mmd_path_list]
             
             # finish the rest of them when its time to upload.
-            csvlist=get_degreefiles_lists(degreenameassetfolder=degreenameassetfolder)[0]
+            csvlist=get_assetcloudpaths_lists(degreenameassetfolder=degreenameassetfolder)[0]
+            print(f'\nCSV LIst is {csvlist}')
 
-
-
-            '''Somewhat Taken from https://cloud.google.com/storage/docs/hosting-static-website#storage-upload-object-python'''
                     
             
                               
